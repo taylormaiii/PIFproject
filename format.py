948aac1d473a8bbabaf00e15d12c5a6d81328ad4
TaylorMai <tjkittykong@gmail.com>
2026-09-02 18:17:05 -0400

    differentiated caught vs hatched mons

--- format.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("output.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-with open("output2.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, indent=2, ensure_ascii=False)
