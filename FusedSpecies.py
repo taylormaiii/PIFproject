5871b54a8aec1ac027238214b8b17b0749831a0d
TaylorMai <tjkittykong@gmail.com>
2026-09-11 20:26:48 -0400

    developing api calls and fusion engine

--- FusedSpecies.py
+++ FusedSpecies.py
@@ -1,274 +1,150 @@
 import pokeapitest
+import requests
 
-def get_body_number_from_symbol(id):
-  return id
-
-
-def get_head_number_from_symbol(id):
-  return id
-
-
-def get_body_species():
-  return body_pokemon.id_number
-
-
-def get_head_species():
-  return head_pokemon.id_number
-
-
-def get_body_species_symbol():
-  return body_pokemon.id
-
-
-def get_head_species_symbol():
-  return head_pokemon.id
-
-
-def adjust_stats_with_evs():
-  GameData::Stat.each_main do |s|
-  base_stats[s.id] = 1 if not base_stats[s.id] || base_stats[s.id] <= 0
-  evs[s.id] = 0 if not evs[s.id] || evs[s.id] < 0
- 
-
+head_pokemon = input("Head pokemon ")
+body_pokemon = input("Body pokemon ")
 
 #FUSION CALCULATIONS
-def calculate_dex_number():
-  return (body_pokemon.id_number * NB_POKEMON) + head_pokemon.id_number
 
+def calculate_id():
+  id1 = pokeapitest.get_id(head_pokemon)
+  id2 = pokeapitest.get_id(body_pokemon)
+  return f"{id1}.{id2}"
 
 def calculate_type1():
-  if head_pokemon.type1 == NORMAL & head_pokemon.type2 == FLYING:
-    return head_pokemon.type2 
-  return head_pokemon.type1
-
+  head_type1, _ = pokeapitest.get_types(head_pokemon)
+  return head_type1
 
 def calculate_type2():
-  if body_pokemon.type2 == type1:
-    return body_pokemon.type1 
-  return body_pokemon.type2
+  head_type1, _ = pokeapitest.get_types(head_pokemon)
+  body_type1, body_type2 = pokeapitest.get_types(body_pokemon)
 
+  if body_type2 == head_type1:
+    body_type2 = body_type1
+  elif body_type2 is None:
+    body_type2 = body_type1
+
+  return None if body_type2 == head_type1 else body_type2
 
 def calculate_base_stats():
-  head_stats = head_pokemon.base_stats
-  body_stats = body_pokemon.base_stats
+  head_stats = pokeapitest.get_bst(head_pokemon)
+  body_stats = pokeapitest.get_bst(body_pokemon)
 
   fused_stats = {}
 
-  #Head dominant stats
-  fused_stats[:HP] = calculate_fused_stats(head_stats[:HP], body_stats[:HP])
-  fused_stats[:SPECIAL_DEFENSE] = calculate_fused_stats(head_stats[:SPECIAL_DEFENSE], body_stats[:SPECIAL_DEFENSE])
-  fused_stats[:SPECIAL_ATTACK] = calculate_fused_stats(head_stats[:SPECIAL_ATTACK], body_stats[:SPECIAL_ATTACK])
 
-  #Body dominant stats
-  fused_stats[:ATTACK] = calculate_fused_stats(body_stats[:ATTACK], head_stats[:ATTACK])
-  fused_stats[:DEFENSE] = calculate_fused_stats(body_stats[:DEFENSE], head_stats[:DEFENSE])
-  fused_stats[:SPEED] = calculate_fused_stats(body_stats[:SPEED], head_stats[:SPEED])
-
+  fused_stats['HP'] = calculate_fused_stats(int(head_stats['HP']), int(body_stats['HP']))
+  fused_stats['ATTACK'] = calculate_fused_stats(int(body_stats['ATTACK']), int(head_stats['ATTACK']))
+  fused_stats['DEFENSE'] = calculate_fused_stats(int(body_stats['DEFENSE']), int(head_stats['DEFENSE']))
+  fused_stats['SPECIAL_ATTACK'] = calculate_fused_stats(int(head_stats['SPECIAL_ATTACK']), int(body_stats['SPECIAL_ATTACK']))
+  fused_stats['SPECIAL_DEFENSE'] = calculate_fused_stats(int(head_stats['SPECIAL_DEFENSE']), int(body_stats['SPECIAL_DEFENSE']))
+  fused_stats['SPEED'] = calculate_fused_stats(int(body_stats['SPEED']), int(head_stats['SPEED']))
+  fused_stats['TOTAL'] = fused_stats['HP'] + fused_stats['ATTACK'] + fused_stats['DEFENSE'] + fused_stats['SPECIAL_ATTACK'] + fused_stats['SPECIAL_DEFENSE'] + fused_stats['SPEED']
   return fused_stats
 
-
 def calculate_moveset():
-  return combine_arrays(body_pokemon.moves, head_pokemon.moves)
-
+  return combine_list(pokeapitest.get_level_moves(head_pokemon), pokeapitest.get_level_moves(body_pokemon))
 
 def calculate_egg_moves():
-  return combine_arrays(body_pokemon.egg_moves, head_pokemon.egg_moves)
+  return combine_list(pokeapitest.get_egg_moves(head_pokemon), pokeapitest.get_egg_moves(body_pokemon))
 
-
-def calculate_tutor_moves():
-  return combine_arrays(body_pokemon.tutor_moves, head_pokemon.tutor_moves)
-
-
-
+def calculate_tm_moves():
+  return combine_list(pokeapitest.get_tm_moves(head_pokemon), pokeapitest.get_tm_moves(body_pokemon))
 
 def calculate_abilities():
   abilities_hash = []
 
-  ability1 = body_pokemon.abilities[0]
-  ability2 = head_pokemon.abilities[0]
-  abilities_hash << ability1
-  abilities_hash << ability2
+  headability1 = pokeapitest.get_abilities(head_pokemon)[0]
+  headability2 = pokeapitest.get_abilities(head_pokemon)[1]
+  bodyability1 = pokeapitest.get_abilities(body_pokemon)[0]
+  bodyability2 = pokeapitest.get_abilities(body_pokemon)[1]
+  abilities_hash.append(headability1)
+  abilities_hash.append(headability2)
+  abilities_hash.append(bodyability1)
+  abilities_hash.append(bodyability2)
   return abilities_hash
 
 def calculate_hidden_abilities():
-  abilities_hash = []
+  hidden_abilities_hash = []
 
   #First two spots are the other abilities of the two pokemon
-  ability1 = body_pokemon.abilities[1]
-  ability2 = head_pokemon.abilities[1]
+  ability1 = pokeapitest.get_abilities(head_pokemon)[2]
+  ability2 = pokeapitest.get_abilities(body_pokemon)[2]
   if not ability1:
-    ability1 = body_pokemon.abilities[0]
+    ability1 = pokeapitest.get_abilities(body_pokemon)[2]
   if not ability2: 
-    ability2 = head_pokemon.abilities[0] 
+    ability2 = pokeapitest.get_abilities(head_pokemon)[2] 
 
-  abilities_hash << ability1
-  abilities_hash << ability2
-
-  #add the hidden ability for the two base pokemon
-  hiddenAbility1 = body_pokemon.hidden_abilities[0]
-  if not hiddenAbility1:
-    hiddenAbility1 = ability1 
-
-  hiddenAbility2 = head_pokemon.hidden_abilities[0]
-  if  not hiddenAbility2:
-    hiddenAbility2 = ability2 
-
-  abilities_hash << hiddenAbility1
-  abilities_hash << hiddenAbility2
-  return abilities_hash
-
-
-def calculate_name():
-  body_nat_dex = GameData::NAT_DEX_MAPPING[body_pokemon.id_number] ? GameData::NAT_DEX_MAPPING[body_pokemon.id_number] : body_pokemon.id_number
-  head_nat_dex = GameData::NAT_DEX_MAPPING[head_pokemon.id_number] ? GameData::NAT_DEX_MAPPING[head_pokemon.id_number] : head_pokemon.id_number
-  prefix = GameData::SPLIT_NAMES[head_nat_dex][0]
-  suffix = GameData::SPLIT_NAMES[body_nat_dex][1]
-  if prefix[-1] == suffix[0]:
-    prefix = prefix[0..-2]
-    
-  return prefix + suffix
+  hidden_abilities_hash.append(ability1)
+  hidden_abilities_hash.append(ability2)
+  return hidden_abilities_hash
 
 def calculate_evolutions():
-  body_evolutions = body_pokemon.evolutions
-  head_evolutions = head_pokemon.evolutions
+  body_evolutions = pokeapitest.get_evos(body_pokemon)
+  head_evolutions = pokeapitest.get_evos(head_pokemon)
 
   fused_evolutions = []
 
   #body
   for evolution in body_evolutions:
-    evolutionSpecies = evolution[0]
-    evolutionSpecies_dex = GameData::Species.get(evolutionSpecies).id_number
-    fused_species = _INTL("B{1}H{2}", evolutionSpecies_dex, head_pokemon.id_number)
-    fused_evolutions << build_evolution_array(evolution, fused_species)
+    fused_evolutions.append(evolution)
   
-
   #head
   for evolution in head_evolutions:
-    evolutionSpecies = evolution[0]
-    evolutionSpecies_dex = GameData::Species.get(evolutionSpecies).id_number
-    fused_species = _INTL("B{1}H{2}", body_pokemon.id_number, evolutionSpecies_dex)
-    fused_evolutions << build_evolution_array(evolution, fused_species)
+    fused_evolutions.append(evolution)
   
-
   return fused_evolutions
 
+def get_types(pokemon):
+    response = requests.get(
+        f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+    ).json()
 
-#Change the evolution species deping if head & body and keep the rest of the data the same
-def build_evolution_array(evolution_data, new_species):
-  fused_evolution_array = []
-  fused_evolution_array << new_species.to_sym
+    types = tuple(
+        entry["type"]["name"]
+        for entry in response["types"]
+    )
 
-  #add the rest
-  for data in evolution_data:
-    if evolution_data.index(data) == 0:
-      fused_evolution_array << data
-  
-  return fused_evolution_array
+    if len(types) == 1:
+        return types[0], None
 
-
-def calculate_category():
-  return split_and_combine_text(body_pokemon.category, head_pokemon.category, " ")
-
+    return types[0], types[1]
 
 #############################  UTIL METHODS ###############################
 
-#Takes 2 strings, splits and combines them using the beginning of the first one and the  of the second one
-# (for example for pokedex entries)
-def split_and_combine_text(beginingText_full, Text_full, separator):
-  beginingText_split = beginingText_full.split(separator, 2)
-  Text_split = Text_full.split(separator, 2)
-
-  beginningText = beginingText_split[0]
-  Text = Text_split[1] & Text_split[1] != "" ? Text_split[1] : Text_split[0]
-  return beginningText + separator + " " + Text
-
 def calculate_fused_stats(dominantStat, otherStat):
-  return ((2 * dominantStat) / 3) + (otherStat / 3).floor
-
+  return (2 * dominantStat + otherStat) // 3
 
 def average_values(value1, value2):
-  return ((value1 + value2) / 2).floor
-
-
-def average_map_values(map1, map2):
-  averaged_map = map1.merge(map2) do |key, value1, value2|
-  ((value1 + value2) / 2.0).floor
-
-  return averaged_map
-
+  return ((value1 + value2) // 2)
 
 def get_highest_value(value1, value2):
-  return value1 > value2 ? value1 : value2
-
+  return max(value1,value2)
 
 def get_lowest_value(value1, value2):
-  return value1 < value2 ? value1 : value2
+  return min(value1, value2)
 
+def combine_list(list1, list2):
+  return zip(list1,list2)
 
-def combine_arrays(array1, array2):
-  return array1 + array2
+id = calculate_id()
+type1 = calculate_type1()
+type2 = calculate_type2()
 
-def initialize(id):
-  if id.is_a?(Integer):
-    body_id = getBodyID(id)
-    head_id = getHeadID(id, body_id)
-    pokemon_id = getFusedPokemonIdFromDexNum(body_id, head_id)
-    return GameData_FusedSpecies.new(pokemon_id)
+#Stats
+#find a way to show stat loss + gain
+base_stats = calculate_base_stats()
 
-  head_id = get_head_number_from_symbol(id)
-  body_id = get_body_number_from_symbol(id)
+#Moves
+moves = calculate_moveset()
+tm_moves = calculate_tm_moves() # hash[:tutor_moves] || []
+egg_moves = calculate_egg_moves() # hash[:egg_moves] || []
 
-  body_pokemon = GameData_Species.get(body_id)
-  head_pokemon = GameData_Species.get(head_id)
+#Abilities
+abilities = calculate_abilities() # hash[:abilities] || []
+hidden_abilities = calculate_hidden_abilities() # hash[:hidden_abilities] || []
 
-  id = id
-  id_number = calculate_dex_number()
-  species = id
-  form = 0
-  real_name = calculate_name()
-  real_form_name = nil
-
-  type1 = calculate_type1()
-  type2 = calculate_type2()
-
-  #Stats
-  base_stats = calculate_base_stats()
-  evs = calculate_evs()
-  adjust_stats_with_evs()
-
-  base_exp = calculate_base_exp()
-  growth_rate = calculate_growth_rate()
-  ger_ratio = calculate_ger() #todo
-  catch_rate = calculate_catch_rate()
-  happiness = calculate_base_happiness()
-
-  #Moves
-  moves = calculate_moveset()
-  tutor_moves = calculate_tutor_moves() # hash[:tutor_moves] || []
-  egg_moves = calculate_egg_moves() # hash[:egg_moves] || []
-
-  #Abilities
-  abilities = calculate_abilities() # hash[:abilities] || []
-  hidden_abilities = calculate_hidden_abilities() # hash[:hidden_abilities] || []
-
-
-
-  evolutions = calculate_evolutions() # hash[:evolutions] || []
-
-  #pokedex
-  pokedex_form = form #ignored
-  real_category = calculate_category()
-  real_pokedex_entry = calculate_dex_entry()
-  height = average_values(head_pokemon.height, body_pokemon.height)
-  weight = average_values(head_pokemon.weight, body_pokemon.weight)
-  color = head_pokemon.color
-  shape = body_pokemon.shape
-
-  #sprite positioning
-  back_sprite_x = body_pokemon.back_sprite_x
-  back_sprite_y = body_pokemon.back_sprite_y
-  front_sprite_x = body_pokemon.front_sprite_x
-  front_sprite_y = body_pokemon.front_sprite_y
-  front_sprite_altitude = body_pokemon.front_sprite_altitude
-  shadow_x = body_pokemon.shadow_x
-  shadow_size = body_pokemon.shadow_size
+evolutions = calculate_evolutions() # hash[:evolutions] || []
+#automatically show both options
+#add a weakness chart
+print(id,type1,type2,base_stats,abilities,evolutions)
--- pokeapitest.py
+++ pokeapitest.py
@@ -2,63 +2,150 @@ import json
 import requests
 
 
-pokemon = input("whats pokemon? ")
-r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}").json()
-re = requests.get(f"https://pokeapi.co/api/v2/pokemon-species/{pokemon}").json()
-evo = requests.get(re['evolution_chain']['url']).json()
-def get_abilities():
-    pokeabilities = [r['abilities'][0]['ability']['name'], r['abilities'][1]['ability']['name']]
-    hiddenabilities = r['abilities'][2]['ability']['name']
-    return f"Regular Abilities: {pokeabilities}, Hidden Abilities: {hiddenabilities}"
+#pokemon = input("whats pokemon? ")
 
-def get_moves():
-    moves = []
-    learn_level = []
-    pairs = []
-    pokemoves = r['moves']
-    moveinfo = []
+
+def get_id(pokemon):
+    with open("pokemon_data.json","r") as pdj:
+        dexdata = json.load(pdj)
+        for data in dexdata:
+            if data['name'].lower() == pokemon:
+                id = data['id']
+    return id
+
+
+def get_species(pokemon):
+    r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}").json()
+    return r['name']
+
+def get_abilities(pokemon):
+    r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}").json()
+    regular_abilities = [
+        entry['ability']['name']
+        for entry in r['abilities']
+        if not entry['is_hidden']
+    ]
+    hidden_abilities = [
+        entry['ability']['name']
+        for entry in r['abilities']
+        if entry['is_hidden']
+    ]
+
+    return (
+        regular_abilities[0] if len(regular_abilities) > 0 else None,
+        regular_abilities[1] if len(regular_abilities) > 1 else None,
+        hidden_abilities[0] if hidden_abilities else None,
+    )
+
+def get_level_moves(pokemon):
+    r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}").json()
     movetotal = []
-    for move in pokemoves:
-        moves.append(move['move']['name'])
+    for move in r['moves']:
         for version in move['version_group_details']:
-            if version['version_group']['name'] == "black-2-white-2":
-                learn_level.append(version['level_learned_at'])
-        movedet = requests.get(f"https://pokeapi.co/api/v2/move/{move['move']['name']}").json()
-        if movedet['accuracy'] is None and movedet['power'] is None:
-            moveinfo.append(f"Non-damaging | accuracy: 100 | pp: {movedet['pp']}")            
-        elif movedet['power'] is None and movedet['accuracy'] is not None:
-            moveinfo.append(f"Non-damaging | accuracy: {movedet['accuracy']} | pp:{movedet['pp']}")
-        elif movedet['accuracy'] is None and movedet['power'] is not None:
-            moveinfo.append(f"Power: {movedet['power']} | accuracy: 100 | pp:{movedet['pp']}")
-        else:
-            moveinfo.append(f"Power: {movedet['power']} | accuracy: {movedet['accuracy']} | pp: {movedet['pp']}")
-    #add physical/special if power
-    for item1, item2 in zip(moves,learn_level):
-        pairs.append(f"{item1}: learned at level {item2}")
-    for item1, item2 in zip(pairs, moveinfo):
-        movetotal.append(f"{item1}: {item2}")
+            if (version['version_group']['name'] != "black-2-white-2" or
+                    version['move_learn_method']['name'] != "level-up"):
+                continue
+
+            move_name = move['move']['name']
+            movedet = requests.get(f"https://pokeapi.co/api/v2/move/{move_name}").json()
+            if movedet['accuracy'] is None and movedet['power'] is None:
+                move_info = f"Non-damaging | accuracy: 100 | pp: {movedet['pp']} | Type: {movedet['type']['name']}"
+            elif movedet['power'] is None and movedet['accuracy'] is not None:
+                move_info = f"Non-damaging | accuracy: {movedet['accuracy']} | pp:{movedet['pp']} | Type: {movedet['type']['name']}"
+            elif movedet['accuracy'] is None and movedet['power'] is not None:
+                move_info = f"Power: {movedet['power']} | accuracy: 100 | pp:{movedet['pp']} | Damage Type: {movedet['damage_class']['name']} | Type: {movedet['type']['name']}"
+            else:
+                move_info = f"Power: {movedet['power']} | accuracy: {movedet['accuracy']} | pp: {movedet['pp']} | Damage Type: {movedet['damage_class']['name']} | Type: {movedet['type']['name']}"
+
+            movetotal.append(
+                f"{move_name}: learned at level {version['level_learned_at']}: {move_info}"
+            )
     return movetotal
 
+def get_egg_moves(pokemon):
+    r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}").json()
+    re = requests.get(f"https://pokeapi.co/api/v2/pokemon-species/{pokemon}").json()
+    evo = requests.get(re['evolution_chain']['url']).json()
+    movetotal = []
+    for move in r['moves']:
+        for version in move['version_group_details']:
+            if (version['version_group']['name'] != "black-2-white-2" or
+                    version['move_learn_method']['name'] != "egg"):
+                continue
+
+            move_name = move['move']['name']
+            movedet = requests.get(f"https://pokeapi.co/api/v2/move/{move_name}").json()
+            if movedet['accuracy'] is None and movedet['power'] is None:
+                move_info = f"Non-damaging | accuracy: 100 | pp: {movedet['pp']} | Type: {movedet['type']['name']}"
+            elif movedet['power'] is None and movedet['accuracy'] is not None:
+                move_info = f"Non-damaging | accuracy: {movedet['accuracy']} | pp:{movedet['pp']} | Type: {movedet['type']['name']}"
+            elif movedet['accuracy'] is None and movedet['power'] is not None:
+                move_info = f"Power: {movedet['power']} | accuracy: 100 | pp:{movedet['pp']} | Damage Type: {movedet['damage_class']['name']} | Type: {movedet['type']['name']}"
+            else:
+                move_info = f"Power: {movedet['power']} | accuracy: {movedet['accuracy']} | pp: {movedet['pp']} | Damage Type: {movedet['damage_class']['name']} | Type: {movedet['type']['name']}"
+
+            movetotal.append(
+                f"{move_name}: learned at level {version['level_learned_at']}: {move_info}"
+            )
+    return movetotal
+
+def get_tm_moves(pokemon):
+    r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}").json()
+    re = requests.get(f"https://pokeapi.co/api/v2/pokemon-species/{pokemon}").json()
+    evo = requests.get(re['evolution_chain']['url']).json()
+    movetotal = []
+    for move in r['moves']:
+        for version in move['version_group_details']:
+            if (version['version_group']['name'] != "black-2-white-2" or
+                    version['move_learn_method']['name'] != "machine"):
+                continue
+
+            move_name = move['move']['name']
+            movedet = requests.get(f"https://pokeapi.co/api/v2/move/{move_name}").json()
+            if movedet['accuracy'] is None and movedet['power'] is None:
+                move_info = f"Non-damaging | accuracy: 100 | pp: {movedet['pp']} | Type: {movedet['type']['name']}"
+            elif movedet['power'] is None and movedet['accuracy'] is not None:
+                move_info = f"Non-damaging | accuracy: {movedet['accuracy']} | pp:{movedet['pp']} | Type: {movedet['type']['name']}"
+            elif movedet['accuracy'] is None and movedet['power'] is not None:
+                move_info = f"Power: {movedet['power']} | accuracy: 100 | pp:{movedet['pp']} | Damage Type: {movedet['damage_class']['name']} | Type: {movedet['type']['name']}"
+            else:
+                move_info = f"Power: {movedet['power']} | accuracy: {movedet['accuracy']} | pp: {movedet['pp']} | Damage Type: {movedet['damage_class']['name']} | Type: {movedet['type']['name']}"
+
+            movetotal.append(
+                f"{move_name}: learned at level {version['level_learned_at']}: {move_info}"
+            )
+    return movetotal
     
 
-def get_types():
-    types = r['types'][0]['type']['name']
-    try:
-        types1 = r['types'][1]['type']['name']
-        return types, types1
-    except IndexError:
-        return types
+def get_types(pokemon):
+    with open("pokemon_data.json","r") as pdj:
+        dexdata = json.load(pdj)
+        for data in dexdata:
+            if data['name'].lower() == pokemon:
+                types = tuple(type_data['name'] for type_data in data['types'])
+                return types[0], types[1] if len(types) > 1 else None
+
+    raise ValueError(f"Unknown Pokemon: {pokemon}")
     
-def get_bst():
+def get_bst(pokemon):
+
+    r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}").json()
+    re = requests.get(f"https://pokeapi.co/api/v2/pokemon-species/{pokemon}").json()
+    evo = requests.get(re['evolution_chain']['url']).json()
     hp = r['stats'][0]['base_stat']
     attack = r['stats'][1]['base_stat']
     defense = r['stats'][2]['base_stat']
     special_attack = r['stats'][3]['base_stat']
     special_defense = r['stats'][4]['base_stat']
     speed = r['stats'][5]['base_stat']
-    return f"HP: {hp}, Attack: {attack}, Defense: {defense}, Special Attack: {special_attack}, Special Defense: {special_defense},Speed: {speed}"
+    statdict = {"HP": f"{hp}", "ATTACK": f"{attack}", "DEFENSE": f"{defense}", "SPECIAL_ATTACK": f"{special_attack}", "SPECIAL_DEFENSE": f"{special_defense}", "SPEED": f"{speed}"}
+    return statdict
 
-def get_evos():
+def get_evos(pokemon):
+#needs to reference pokemon_data instead of the api
+    r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}").json()
+    re = requests.get(f"https://pokeapi.co/api/v2/pokemon-species/{pokemon}").json()
+    evo = requests.get(re['evolution_chain']['url']).json()
     try:
         evoinfo = [evo['chain']['species']['name'],evo['chain']['evolves_to'][0]['species']['name'],evo['chain']['evolves_to'][0]['evolves_to'][0]['species']['name']]
     except IndexError:
@@ -68,4 +155,4 @@ def get_evos():
             evoinfo = evo['chain']['species']['name']
     return evoinfo
 
-print(get_moves())
+#print(get_bst(pokemon))
