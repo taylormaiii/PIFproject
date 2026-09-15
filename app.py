856ddb3c5ff14c095d0fc63e5463cc076bc42bfb
TaylorMai <tjkittykong@gmail.com>
2026-09-14 22:13:34 -0400

    flask html skeleton

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import requests
+import json
 from flask import Flask, render_template, request
 import fusion.FusedSpecies as fs 
 
 app = Flask(__name__)
 
+with open("data/locations.json", encoding="utf-8") as locations_file:
+    locations = json.load(locations_file)
 
 @app.route("/", methods=['GET', 'POST'])
 def fusion():
@@ -13,4 +16,5 @@ def fusion():
         result = fs.calculate_fusion(head, body)
         return render_template("results_data.html", result=result)
 
-    return render_template("form.html")
+    return render_template("base.html", locations=locations)
+
